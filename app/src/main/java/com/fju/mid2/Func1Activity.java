70a7c7fd7f565435c4f246b4173d62e43d166800
huaijung Huang <je3rjoy@gmail.com>
2017-12-08 19:21:24 +0800

    change

--- app/src/main/java/com/fju/mid2/Func1Activity.java
+++ app/src/main/java/com/fju/mid2/Func1Activity.java
@@ -1,8 +1,11 @@
 package com.fju.mid2;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.Random;
 
 public class Func1Activity extends AppCompatActivity {
 
@@ -10,6 +13,16 @@ public class Func1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_func1);
+    }
+
+    public void number(View view){
+        Random r = new Random();
+        int num = r.nextInt(6)+1;
+        new AlertDialog.Builder(this)
+                .setTitle("dice")
+                .setMessage(""+num)
+                .setPositiveButton("OK", null)
+                .show();
     }
 
 }
